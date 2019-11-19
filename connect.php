0b80b1354727af3483f0ccad3f238d2274d55b7a
esthook <57952302+esthook@users.noreply.github.com>
2019-11-19 21:32:56 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,52 @@
+<?php
+
+	//@mysql_connect("localhost","dbsucces_dbsuccess","dbsuccess1234");
+	//mysql_select_db("dbsucces_dbsuccess")or die(mysql_error());
+	mysql_connect("localhost","root","1234");
+	mysql_select_db("mill88")or die(mysql_error());
+	mysql_query("SET NAME UTF8");
+	mysql_query("SET character_set_results=utf8");
+	mysql_query("SET character_set_client='utf8' ");
+	mysql_query("SET character_set_connection='utf8' ");
+	mysql_query("collation_connection = utf8_unicode_ci");
+	mysql_query("collation_database = utf8_unicode_ci");
+	mysql_query("collation_server = utf8_unicode_ci");
+	
+$www="dbsuccess.net";
+$mainname="dbsuccess.net";
+$maintel="-";
+$mainemail="info@dbsuccess.net";
+/* FTP Hostname: dbsuccess.net หรือ 119.59.104.31
+FTP Username: dbsucces
+FTP Password: 0Zkd620mRs
+FTP Port : 2002 
+
+
+Database:	dbsucces_dbsuccess
+Host:	localhost
+Username:	dbsucces_dbsuccess
+Password:	dbsuccess1234
+
+Database:	admin_dbsuccess
+Host:	localhost
+Username:	admin_dbsuccess
+Password:	dbsuccess1234
+
+Cloud VPS Linux HD 200 GB / Ram 4 GB
+
+Main IP: 119.59.116.203
+
+DirectAdmin
+http://119.59.116.203:2222
+
+Username : admin
+Password : 71f4db2f301697a
+
+Shell SSH
+
+IP Address : 119.59.116.203
+Username : root
+Password : 71f4db2f301697a
+	ns1.hostinglotus.com
+*/
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	
+	
+	//header("location:member/login.php");
+?>
+<!doctype html>
+<?require('config/connect.php'); require('config/function.php'); ?>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>7FSNETWORK.COM</title>
+</head>
+<body>
+	<div style="width:100%;margin:0 auto;display:block;">
+	<div style="float:right;"><a href="member/login.php"><img src="liner/images/member_login_th.gif"></a></div>
+	<div style="clear:both;"></div>
+	<center><h1 style="color: red;text-shadow: 0 0 0.2em #A6A6A9, 0 0 0.2em ;font-size:45px;">WWW.7FSNETWORK.COM</h1></center>
+
+		
+		<center><img src="head.jpg"></center>
+	
+	
+	</div>
+	<!--<div style="width:1280px;margin:0 auto;display:block;border:solid 1px #eee;border-radius:5px;">
+	<? $gj=queryx1("select  sa_detail from system_article where sa_id='8' limit 1"); ?>
+	<?=$gj['sa_detail'];?>
+	</div>-->
+
+</body>
+<html>
